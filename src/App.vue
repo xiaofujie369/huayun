32bf91c4c198ff87f0536d32c35bce66c04af8cd
xiaofujie369 <132354004+xiaofujie369@users.noreply.github.com>
2025-11-25 14:21:48 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,19 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+  background: #f7f9fc;
+  font-family: "Inter", "PingFang SC", "Microsoft YaHei", sans-serif;
+}
+</style>
 
