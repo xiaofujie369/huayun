8bc5b6c911d5dd0ebe124bd1d318c66e7070be27
xiaofujie369 <132354004+xiaofujie369@users.noreply.github.com>
2025-11-25 14:28:44 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,13 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+  },
+});
 
