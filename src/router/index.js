cd60396442dbd327d5c000de7f3ed2470c483fb8
xiaofujie369 <132354004+xiaofujie369@users.noreply.github.com>
2025-11-25 14:22:21 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Register from "../pages/Register.vue";
+import Login from "../pages/Login.vue";
+import Gallery from "../pages/Gallery.vue";
+import Upload from "../pages/Upload.vue";
+import PhotoView from "../pages/PhotoView.vue";
+import ShareView from "../pages/ShareView.vue";
+
+const routes = [
+  { path: "/", redirect: "/gallery" },
+  { path: "/register", component: Register },
+  { path: "/login", component: Login },
+  { path: "/gallery", component: Gallery },
+  { path: "/upload", component: Upload },
+  { path: "/photo/:id", component: PhotoView },
+  { path: "/share/:key", component: ShareView },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
 
